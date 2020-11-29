7e84616edadf82006dbc9dd47fbf5d44f7985194
Shamilprog <shamilprog@yandex.ru>
2020-11-29 23:32:06 +0300

    Initial commit

---
+++ app/src/main/java/ru/shamilprog/android/beatbox/BeatBoxActivity.java
@@ -0,0 +1,13 @@
+package ru.shamilprog.android.beatbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeatBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
