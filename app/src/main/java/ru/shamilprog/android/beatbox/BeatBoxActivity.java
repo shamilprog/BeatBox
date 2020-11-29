ae2803a7570688107b0d3f29c3b382ee3dcd63c5
Shamilprog <shamilprog@yandex.ru>
2020-11-30 00:08:24 +0300

    Create a fragment, make activity to be host activity for it.

--- app/src/main/java/ru/shamilprog/android/beatbox/BeatBoxActivity.java
+++ app/src/main/java/ru/shamilprog/android/beatbox/BeatBoxActivity.java
@@ -1,13 +1,13 @@
 package ru.shamilprog.android.beatbox;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BeatBoxActivity extends AppCompatActivity {
+public class BeatBoxActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
     }
 }
---
+++ app/src/main/java/ru/shamilprog/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,11 @@
+package ru.shamilprog.android.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+
+}
