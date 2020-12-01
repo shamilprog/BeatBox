05ce0d71d3671901ce9fb5a8df74964e5021643b
Shamilprog <shamilprog@yandex.ru>
2020-12-01 22:42:39 +0300

    Create SoundViewModel class.

---
+++ app/src/main/java/ru/shamilprog/android/beatbox/SoundViewModel.java
@@ -0,0 +1,22 @@
+package ru.shamilprog.android.beatbox;
+
+public class SoundViewModel {
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+    }
+}
