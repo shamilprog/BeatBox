768332201b13e050296323f00924dd47a4fa40a5
Shamilprog <shamilprog@yandex.ru>
2020-12-05 23:13:37 +0300

    Settings up done for unit testing.

---
+++ app/src/test/java/ru/shamilprog/android/beatbox/SoundViewModelTest.java
@@ -0,0 +1,27 @@
+package ru.shamilprog.android.beatbox;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SoundViewModelTest {
+    private BeatBox mBeatBox;
+    private Sound mSound;
+    private SoundViewModel mSubject;
+
+    @Before
+    public void setUp() throws Exception {
+        mBeatBox = Mockito.mock(BeatBox.class);
+        mSound = new Sound("assetPath");
+        mSubject = new SoundViewModel(mBeatBox);
+        mSubject.setSound(mSound);
+    }
+
+    @Test
+    public void exposesSoundNameAsTitle() {
+        assertThat(mSubject.getTitle(), is(mSound.getName()));
+    }
+}
